c0de44ed1e8e11e0d49fa34315cc84bbac23ccd5
kaylaobritsch <kaylaobritsch@MacBook-Pro.local>
2017-05-05 16:12:49 -0400

    Declared variables right
    Used switch to change user stories.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
+var color;
+var creature;
+var adjective;
+var verbed;
+var story;
+
 alert('Let’s make a story together!');
 
-var color = prompt('A colour (orange, indigo, etc.)');
-var creature = prompt('A creature (dragon, moth, etc.)');
-var adjective = prompt('An adjective (beautiful, super, etc.)');
-var verbed = prompt('A past tense verb (ran, burped, etc.)');
-var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) \nWinning Enter the letter below:');
+color = prompt('A colour (orange, indigo, etc.)');
+creature = prompt('A creature (dragon, moth, etc.)');
+adjective = prompt('An adjective (beautiful, super, etc.)');
+verbed = prompt('A past tense verb (ran, burped, etc.)');
+story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\nEnter the letter below:');
 
 switch (story) {
   case 'a':
-    alert('After snacking on ' + adjective + ' treats, the ' + color + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+    document.write('After snacking on ' + adjective + ' treats, the ' + color + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+    break;
 
   case 'b':
-    alert('Amelia ' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
+    document.write('Amelia ' + verbed + ' through the ' + adjective + ' ' + color + ' nebula escaping the space ' + creature + '.');
+    break;
 
-  case 'c':
-    alert('Jackson chose his ' +  adjective + ',' + colour + ' ' + creature + ' card and ' + verbed + 'it to the table knowing he won.');
+  case 'c' :
+    document.write('Jackson chose his ' +  adjective + ',' + color + ' ' + creature + ' card and ' + verbed + 'it to the table knowing he won.');
 }
