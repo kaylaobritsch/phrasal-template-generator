3aebfbc77b0ab747256c5124b36eff47254bfa89
kaylaobritsch <kaylaobritsch@MacBook-Pro.local>
2017-05-05 15:38:57 -0400

    Added the stories

--- js/main.js
+++ js/main.js
@@ -4,44 +4,15 @@ var color = prompt('A colour (orange, indigo, etc.)');
 var creature = prompt('A creature (dragon, moth, etc.)');
 var adjective = prompt('An adjective (beautiful, super, etc.)');
 var verbed = prompt('A past tense verb (ran, burped, etc.)');
-var story = prompt('Which story would you like? a) Snacking b) Escaping c) Winning Enter the letter below:');
+var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) \nWinning Enter the letter below:');
 
-switch(story) {
+switch (story) {
   case 'a':
-    text = 'After snacking on' + adjective + ' treats, the' + color + 'bellied ' + creature + verbed + ' for hours.';
+    alert('After snacking on ' + adjective + ' treats, the ' + color + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+
+  case 'b':
+    alert('Amelia ' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
+
+  case 'c':
+    alert('Jackson chose his ' +  adjective + ',' + colour + ' ' + creature + ' card and ' + verbed + 'it to the table knowing he won.');
 }
-
-
-/*
-
-A colour (orange, indigo, etc.)
-
-
-
-A creature (dragon, moth, etc.)
-
-
-
-An adjective (beautiful, super, etc.)
-
-
-
-A past tense verb (ran, burped, etc.)
-
-
-
-Which story would you like?
-a) Snacking
-b) Escaping
-c) Winning
-
-Enter the letter below:
-
-
-
-After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
-
-Amelia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
-
-Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
-*/
